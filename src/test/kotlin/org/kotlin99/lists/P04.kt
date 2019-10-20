1dda12ebbf1f03c9a99d79f2857ab0d303a00620
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2019-10-20 20:07:56 +0100

    Convert recursive exercises to tail recursion

--- src/test/kotlin/org/kotlin99/lists/P04.kt
+++ src/test/kotlin/org/kotlin99/lists/P04.kt
@@ -5,8 +5,8 @@ import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 import org.kotlin99.common.tail
 
-fun <T> length(list: List<T>): Int =
-    if (list.isEmpty()) 0 else 1 + length(list.tail())
+tailrec fun <T> length(list: List<T>, count: Int = 0): Int =
+    if (list.isEmpty()) count else length(list.tail(), count + 1)
 
 class P04Test {
     @Test fun `find the number of elements of a list`() {
