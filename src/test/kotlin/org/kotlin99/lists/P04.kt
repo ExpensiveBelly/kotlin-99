8f13c50e0c9b1ddca02ce0aab53a3e1e35107169
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-10-18 16:31:51 +0100

    DeepRecursiveFunction

--- src/test/kotlin/org/kotlin99/lists/P04.kt
+++ src/test/kotlin/org/kotlin99/lists/P04.kt
@@ -5,11 +5,24 @@ import com.natpryce.hamkrest.equalTo
 import org.junit.Test
 import org.kotlin99.common.tail
 
+@ExperimentalStdlibApi
+val <T> List<T>.length
+    get() = DeepRecursiveFunction<List<T>, Int> { list ->
+        if (list.isEmpty()) 0 else 1 + callRecursive(list.tail())
+    }(this)
+
 tailrec fun <T> length(list: List<T>, count: Int = 0): Int =
     if (list.isEmpty()) count else length(list.tail(), count + 1)
 
 class P04Test {
-    @Test fun `find the number of elements of a list`() {
+    @Test
+    fun `find the number of elements of a list`() {
+        assertThat(length(listOf<Int>()), equalTo(0))
+        assertThat(length(listOf(1, 1, 2, 3, 5, 8)), equalTo(6))
+    }
+
+    @Test
+    fun `length deep recursive`() {
         assertThat(length(listOf<Int>()), equalTo(0))
         assertThat(length(listOf(1, 1, 2, 3, 5, 8)), equalTo(6))
     }
--- src/test/kotlin/org/kotlin99/lists/P05.kt
+++ src/test/kotlin/org/kotlin99/lists/P05.kt
@@ -6,13 +6,28 @@ import org.junit.Test
 import org.kotlin99.common.tail
 
 tailrec fun <T> reverse(list: List<T>, newList: List<T> = emptyList()): List<T> =
-        when {
-            list.isEmpty() -> emptyList()
-            list.size == 1 -> listOf(list.first()) + newList
-            else -> reverse(list.tail(), listOf(list.first()) + newList)
-        }
+    when {
+        list.isEmpty() -> emptyList()
+        list.size == 1 -> listOf(list.first()) + newList
+        else -> reverse(list.tail(), listOf(list.first()) + newList)
+    }
+
+@ExperimentalStdlibApi
+fun <T> List<T>.reverseDeepRecursive() =
+    DeepRecursiveFunction<List<T>, List<T>> { list ->
+        if (list.size <= 1) list
+        else callRecursive(list.tail() + list.first())
+    }(this)
 
 class P05Test {
+    @ExperimentalStdlibApi
+    @Test
+    fun `reverse a list deep recursive`() {
+        assertThat(listOf<Int>().reverseDeepRecursive(), equalTo(emptyList()))
+        assertThat(listOf(1).reverseDeepRecursive(), equalTo(listOf(1)))
+        assertThat(listOf(1, 1, 2, 3, 5, 8).reverseDeepRecursive(), equalTo(listOf(8, 5, 3, 2, 1, 1)))
+    }
+
     @Test
     fun `reverse a list`() {
         assertThat(reverse(listOf<Int>()), equalTo(emptyList()))
