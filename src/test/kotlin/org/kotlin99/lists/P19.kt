266947f362d4e5117785e3dd8a808ab6dd4c5b17
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-10-28 18:28:07 +0000

    Use `slice` to rotate

--- src/test/kotlin/org/kotlin99/lists/P19.kt
+++ src/test/kotlin/org/kotlin99/lists/P19.kt
@@ -11,8 +11,16 @@ fun <T> rotate(n: Int, list: List<T>): List<T> =
         else   -> list.takeLast(-n) + list.dropLast(-n)
     }
 
+fun <T> rotateBySlicing(n: Int, list: List<T>): List<T> =
+        if (n < 0) list.slice((list.size - (-n)) until list.size) + list.slice (0 until (list.size - (-n)))
+        else list.slice(n until list.size) + list.slice(0 until n)
+
 class P19Test {
-    @Test fun `rotate a list N places to the left`() {
+    @Test
+    fun `rotate a list N places to the left`() {
+        assertThat(rotateBySlicing(3, "abcdefghijk".toList()), equalTo("defghijkabc".toList()))
+        assertThat(rotateBySlicing(-2, "abcdefghijk".toList()), equalTo("jkabcdefghi".toList()))
+
         assertThat(rotate(3, "abcdefghijk".toList()), equalTo("defghijkabc".toList()))
         assertThat(rotate(-2, "abcdefghijk".toList()), equalTo("jkabcdefghi".toList()))
     }
